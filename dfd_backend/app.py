aea19fbb6e3ff0809d1f84d40cd0eda9c2c2b6f0
abhinavgadekar <abhinavgadekar13@gmail.com>
2025-03-26 22:59:47 +0530

    Fixed bugs

--- dfd_backend/app.py
+++ dfd_backend/app.py
@@ -103,5 +103,5 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)  # Ensure correct port
