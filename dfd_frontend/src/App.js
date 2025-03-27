6f49c8ab4a7224a35e8d43d1f89722b929454b9f
abhinavgadekar <abhinavgadekar13@gmail.com>
2025-03-27 18:34:08 +0530

    update

--- dfd_frontend/src/App.js
+++ dfd_frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
      <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
         <Route path='/AboutModel' element={<AboutModel />} />
         <Route path='/Footer' element={<Footer />} />
         <Route path='/Results' element={<Results/>}/>
