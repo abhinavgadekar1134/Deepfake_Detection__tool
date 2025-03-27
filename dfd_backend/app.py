4e01c7cafe50b11f506cd2a392b9cb65f6ac8656
abhinavgadekar <abhinavgadekar13@gmail.com>
2025-03-27 09:25:23 +0530

    Fixed bugs

--- dfd_backend/app.py
+++ dfd_backend/app.py
@@ -105,7 +105,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Correct syntax
- # Use the PORT environment variable
-    print(port)
-    app.run(host='0.0.0.0', port=port) 
+    port = os.environ.get("PORT")
+    app.run(port=port)
