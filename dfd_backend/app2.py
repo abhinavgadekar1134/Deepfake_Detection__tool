033c41beb10ae88c91a967d2a197e07e98db574e
abhinavgadekar <abhinavgadekar13@gmail.com>
2025-03-27 09:13:38 +0530

    Fixed bugs

--- dfd_backend/app2.py
+++ dfd_backend/app2.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,8 @@ def home():
     return "Hello, Render is working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = os.environ.get("PORT")
+    app.run(host="0.0.0.0", port=port)
+
+
+# flask-cors tensorflow opencv-python numpy gdown pillow 
